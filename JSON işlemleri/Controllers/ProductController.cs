d8ca647ef92e72c6ae56be4cbd5d59a40e6368bb
Sefa Dudu <77778888+SefaDudu@users.noreply.github.com>
2021-08-11 20:52:31 +0300

    Add project files.

---
+++ JSON işlemleri/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using JSON_işlemleri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSON_işlemleri.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                Id=1,
+                ProductName="sefa"
+            },
+             new Product()
+            {
+                Id=2,
+                ProductName="kaan"
+            }
+        };
+
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Add(Product product)
+        {
+            products.Add(product);
+            return Json(true);
+        }
+        public JsonResult Delete(int id)
+        {
+            products.RemoveAll(x => x.Id == id);
+            return Json(true,JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Update(int id,Product product)
+        {
+            products.FirstOrDefault(x => x.Id == id).ProductName = product.ProductName;
+
+
+            return Json(true);
+        }
+        public JsonResult GetById(int id)
+        {
+            
+            return Json(products.FirstOrDefault(x => x.Id == id), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetAll()
+        {
+
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ JSON işlemleri/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSON_işlemleri.MvcApplication" Language="C#" %>
